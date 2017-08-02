635e1576814f5e1ece10f1eec7b3886f866da054
callmekohei <callmekohei@Mochidas-MacBook-Air.local>
2017-08-02 20:25:36 +0900

    fix create folder

--- install.bash
+++ install.bash
@@ -3,6 +3,6 @@
 git clone --depth 1 'https://github.com/callmekohei/deoplete-fsharp-bin.git'
 mv deoplete-fsharp-bin ftplugin
 
-touch syntax
+mkdir syntax
 wget 'https://raw.githubusercontent.com/fsharp/vim-fsharp/master/syntax/fsharp.vim' -P './syntax/'
 
