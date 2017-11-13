e14486eaf746c7951c06dc7400ea044c99095222
callmekohei <callmekohei@gmail.com>
2017-11-13 19:59:44 +0900

    change win32 repositry

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-git clone --depth 1 https://github.com/callmekohei/deoplete-fsharp-bin.git
+git clone --depth 1 https://github.com/callmekohei/deopletefs_win32
 move deoplete-fsharp-bin ftplugin
 
 powershell -Command "(new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/fsharp/vim-fsharp/master/syntax/fsharp.vim', 'fsharp.vim')"
