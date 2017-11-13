d3146369193bd3e7660a3831ca3df9a70ca5be00
callmekohei <callmekohei@gmail.com>
2017-11-13 09:00:02 +0900

    curl ---> powershell

--- install.cmd
+++ install.cmd
@@ -3,7 +3,7 @@
 git clone --depth 1 https://github.com/callmekohei/deoplete-fsharp-bin.git
 move deoplete-fsharp-bin ftplugin
 
-curl -O https://raw.githubusercontent.com/fsharp/vim-fsharp/master/syntax/fsharp.vim
+powershell -Command "(new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/fsharp/vim-fsharp/master/syntax/fsharp.vim', 'fsharp.vim')"
 mkdir syntax
 move fsharp.vim syntax
 
