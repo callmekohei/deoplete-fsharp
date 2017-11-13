9010514781068f449f67165ac6a26a4d3f8a3f94
callmekohei <callmekohei@gmail.com>
2017-11-13 20:01:48 +0900

    fix

--- install.cmd
+++ install.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 git clone --depth 1 https://github.com/callmekohei/deopletefs_win32
-move deoplete-fsharp-bin ftplugin
+move deopletefs_win32 ftplugin
 
 powershell -Command "(new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/fsharp/vim-fsharp/master/syntax/fsharp.vim', 'fsharp.vim')"
 mkdir syntax
