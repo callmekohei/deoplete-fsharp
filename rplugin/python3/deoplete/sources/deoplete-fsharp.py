5e5fa3f880e1d46a2bff0904d111b4675b9cb4e8
callmekohei <callmekohei@gmail.com>
2018-05-14 12:57:03 +0900

    base64String from deopletefs

--- rplugin/python3/deoplete/sources/deoplete-fsharp.py
+++ rplugin/python3/deoplete/sources/deoplete-fsharp.py
@@ -5,6 +5,7 @@
 # ===========================================================================
 
 import atexit
+import base64
 import functools
 import os
 import queue
@@ -30,48 +31,43 @@ class Source(Base):
         self.name      = 'deopletefs'
         self.mark      = '[deopletefs]'
         self.filetypes = ['fsharp']
-
-        # work around
-        if self.vim.eval("&filetype") in self.filetypes:
-            self.vim.command("call deoplete#send_event('InsertEnter')")
+        self.standby   = False
 
 
     def on_init(self, context):
 
         self.filePath = expand( self.vim.eval( "substitute( expand('%:p:r') . '_deoplete-fsharp_temporary_file.fsx' , '\#', '\\#' , 'g' )" ) )
-        fsc_path      = expand( re.split('rplugin', __file__)[0] + expand('ftplugin/bin_deopletefs/deopletefs.exe') )
+        fsc_path      = expand( re.split('rplugin', __file__)[0] + expand('ftplugin/bin/deopletefs.exe') )
 
-        # input pattern
+        ### input pattern
         dotHints           = [ r"(\(|<|[a-zA-Z]|\"|\[)*(?<=(\)|>|[a-zA-Z0-9]|\"|\]))\." ]
         oneWordHints       = [ r"^[a-zA-Z]$", "\s*[a-zA-Z]$", "typeof\<[a-zA-Z]$", "(\(\))[a-zA-Z]$", "(\<|\>)[a-zA-Z]$", "(\[|\])[a-zA-Z]$"  ]
         attributeHints     = [ r"\[<[a-zA-Z]*" ]
         self.input_pattern = '|'.join( dotHints + oneWordHints + attributeHints )
 
-        # first initialize of deopletefs
-        post_data = {
-              "Row"      : -9999 # dummy row
-            , "Col"      : -9999 # dummy col
-            , "Line"     : ''    # dummy line
-            , "FilePath" : self.filePath
-            , "Source"   : '\n'.join( getlines( self.vim ) )
-            , "Init"     : 'dummy_init'
-        }
-
-        self.util = Util(fsc_path, 20, json.dumps(post_data))
-        self.util.read()
-
-        # second initialize of deopletefs
+        ### initialize of deopletefs
         post_data = {
               "Row"      : -9999 # dummy row
             , "Col"      : 1
             , "Line"     : ''
             , "FilePath" : self.filePath
             , "Source"   : '\n'.join( getlines( self.vim ) )
-            , "Init"     : 'real_init'
+            , "Init"     : 'true'
         }
 
+        self.util = Util(fsc_path, 20)
         self.util.send(json.dumps(post_data))
-        self.util.read()
+
+        start = time.time()
+        self.vim.command("echo '*** deopletefs initializing... ***'")
+
+        if str(self.util.read()) != '':
+            self.standby = True
+            elapsed_time = time.time() - start
+            self.vim.command("echo '*** finish initialize! *** ( time : " + str(round(elapsed_time,6)) + " s )'")
+        else:
+            elapsed_time = time.time() - start
+            self.vim.command("echo '*** Sorry! Please Re-open file! *** ( time : " + str(round(elapsed_time,6)) + " s )'")
 
 
     def get_complete_position(self, context):
@@ -80,26 +76,34 @@ class Source(Base):
 
 
     def gather_candidates(self, context):
+
         try:
 
-            post_data = {
-                  "Row"      : context['position'][1]
-                , "Col"      : context['complete_position'] - 1
-                , "Line"     : context['input']
-                , "FilePath" : self.filePath
-                , "Source"   : '\n'.join( getlines( self.vim ) )
-                , "Init"     : 'false'
-            }
-
-            self.util.send(json.dumps(post_data))
-
-            return [
-                {
-                      "word": json_data['word']
-                    , "info": '\n'.join( functools.reduce ( lambda a , b : a + b , json_data['info'] ) )
+            if self.standby == False:
+                return [ '=== can not initialize deopletefs ===' ]
+            else:
+                post_data = {
+                      "Row"      : context['position'][1]
+                    , "Col"      : context['complete_position'] - 1
+                    , "Line"     : context['input']
+                    , "FilePath" : self.filePath
+                    , "Source"   : '\n'.join( getlines( self.vim ) )
+                    , "Init"     : 'false'
                 }
-                for json_data in [ json.loads(s) for s in self.util.read() ]
-            ]
+
+                self.util.send(json.dumps(post_data))
+                s = (self.util.read())[0]
+                s = base64.b64decode(s)
+                s = s.decode(encoding='utf-8')
+                lst = s.split("\n")
+
+                return [
+                    {
+                          "word": json_data['word']
+                        , "info": '\n'.join( functools.reduce ( lambda a , b : a + b , json_data['info'] ) )
+                    }
+                    for json_data in [ json.loads(s) for s in lst ]
+                ]
 
         except Exception as e:
             return [ str(e) ]
@@ -107,24 +111,19 @@ class Source(Base):
 
 class Util():
 
-    def __init__(self, exe_path, timeOut_s, txt):
-
+    def __init__(self, exe_path, timeOut_s):
         self.exe_path  = exe_path
-        self.txt       = txt
         self.timeOut_s = timeOut_s
         self.event     = threading.Event()
         self.lines     = queue.Queue()
 
-        # launch deopletefs
+        ### launch deopletefs
         command      = [ 'mono', self.exe_path ]
         opts         = { 'stdin': subprocess.PIPE, 'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE, 'universal_newlines': True }
         self.process = subprocess.Popen( command , **opts )
         atexit.register(lambda: self.process.kill())
 
-        # initialize deopletefs
-        self.send(self.txt)
-
-        # create worker thread
+        ### create worker thread
         self.worker        = threading.Thread(target=self.work, args=(self,))
         self.worker.daemon = True
         self.worker.start()
